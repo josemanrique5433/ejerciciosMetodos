de57f47145b6c311d2e3d5bdbde172811e1457b5
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-02-23 07:36:55 -0500

    Mi primer metodo

---
+++ ejerciciosMetodos/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        sumaNumeros(7, 9);    
+
+    }
+    
+    static void sumaNumeros(int num1,int num2)
+    {
+
+        Console.WriteLine($"La suma de los números es: {num1+num2}");
+
+    }
+    
+}
