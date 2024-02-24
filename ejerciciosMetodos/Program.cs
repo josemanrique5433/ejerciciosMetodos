b969f32ffb888190fe3071f1950c2e7f764a5f8a
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-02-24 08:00:47 -0500

    valores opcionales

--- ejerciciosMetodos/Program.cs
+++ ejerciciosMetodos/Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    static void Main(string[] args)
+    /*static void Main(string[] args)
     {
 
         Console.WriteLine(suma(7,5.3));
@@ -12,6 +12,40 @@ class Program
 
      static int suma(int numero1, double numero2) => numero1;
      static int suma(int numero1, int numero2, int numero3,int numero4) => numero1 + numero2+numero3+numero4;
+    */
+
+    /*static void Main(string[] args)
+    {
+
+        Console.WriteLine(Suma(7, 5.3));
+    }
+    private static double Suma(int v1, double v2)
+    {
+        return v1 + v2;
+    }*/
+
+    //Valores opcionales
+
+    static void main(string[] args)
+    {
+        int valor1 = 5;
+        double valor2 = 7;
+        double valor3 = 2;
+        Console.WriteLine(suma(valor1, valor2, valor3));
+
+    }
+
+    static double suma(int num1, double num2, double num3 = 0)
+    {
+        return num1 + num2 + num3;
+
+    }
+
+    static double suma(int num1, double num2)
+    {
+        return num1 + num2;
+
+    }
 
 
 }
